6ceb9915c18cbd0438d65e1e4c158843412f4929
Vaishnavi Agnihotri <vaishnaviagnihotri40@gmail.com>
2023-06-08 01:58:34 +0530

    Create Merge Two Sorted Arrays.py

---
+++ Day2/Merge Two Sorted Arrays.py
@@ -0,0 +1,26 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+def ninjaAndSortedArrays(nums1,nums2,m,n):
+    # Write your code here.
+        i=m-1
+        j=n-1
+        k=m+n-1
+        
+        while i>=0 and j>=0:
+            if nums1[i]<nums2[j]:
+                nums1[k]=nums2[j]
+                j-=1
+            else:
+                nums1[k]=nums1[i]
+                i-=1
+            k-=1
+        
+        while j>=0:
+            nums1[k]=nums2[j]
+            j-=1
+            k-=1
+
+        return nums1
