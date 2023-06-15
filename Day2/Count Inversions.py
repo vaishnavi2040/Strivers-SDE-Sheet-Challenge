40398172253ecdbd1b1d9f6d43e1cdf190cd32ab
Vaishnavi Agnihotri <vaishnaviagnihotri40@gmail.com>
2023-06-15 07:30:28 +0530

    Create Count Inversions.py

---
+++ Day2/Count Inversions.py
@@ -0,0 +1,23 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def getInversions(arr, n) :
+	# Write your code here. 
+    count=0
+    for i in range(n):
+        for j in range(i,n):
+            if arr[i]>arr[j]:
+                count+=1
+    return count
+
+# Taking inpit using fast I/O.
+def takeInput() :
+    n = int(input())
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+# Main.
+arr, n = takeInput()
+print(getInversions(arr, n))
